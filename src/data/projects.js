a3cf92f04732c2fc6b72ab3144c0cdb1aba54c9c
Marina Ziembar <marinaziembar@gmail.com>
2023-07-29 12:31:28 -0300

    Avances sobre proyectos

---
+++ src/data/projects.js
@@ -0,0 +1,103 @@
+import prisma from '../images/projects/prisma.svg';
+import theDailyPerspective from '../images/projects/the-daily-perspective.svg';
+import replay from '../images/projects/replay.svg';
+import portfolio from '../images/logo.svg';
+import trueNomad from '../images/projects/true-nomad.png';
+
+export const projects = [
+    {
+        title:"Máxima s.r.l",
+        subtitle:"Sitio web institucional para empresa de automatización de procesos industriales.",
+        web:"https://maximasrl.com.ar/",
+        image:"",
+        tags:["Wordpress", "Illustrator", "Photoshop"]
+    },
+    {
+        title:"Kromos Laboratorio",
+        subtitle:"Sitio web institucional para laboratorio de citogenética.",
+        web:"https://www.kromoslab.com.ar/",
+        github:"https://github.com/MarinaZiembar/kromosLaboratorio",
+        image:"",
+        tags:["HTML", "CSS", "Javascript", "Illustrator", "Photoshop"]
+    },
+    {
+        title:"Ada Lovelace - Tribute page",
+        subtitle:"Sitio web de tributo a Ada Lovelace. Proyecto para Free Code Camp.",
+        web:"https://marinaziembar.github.io/FCC-Tribute-Page/",
+        github:"https://github.com/MarinaZiembar/FCC-Tribute-Page",
+        image:"",
+        tags:["HTML", "CSS", "FCC"]
+    },
+    {
+        title:"Finding North",
+        subtitle:"Landing page sobre Islandia. Proyecto para Free Code Camp.",
+        web:"https://marinaziembar.github.io/FCC-Product-Landing-Page/",
+        github:"https://github.com/MarinaZiembar/FCC-Product-Landing-Page",
+        image:"",
+        tags:["HTML", "CSS", "FCC"]
+    },
+    {
+        title:"Oshun - The game",
+        subtitle:"Juego interactivo. Proyecto final de la materia 'Diseño 1' de la UBA.",
+        web:"https://marinaziembar.github.io/OshunTheGame/",
+        github:"https://github.com/MarinaZiembar/OshunTheGame",
+        image:"",
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+    {
+        title:"True Nomad",
+        subtitle:"Sistema de gestión de reserva de hoteles. Primer proyecto de Acámica.",
+        web:"https://marinaziembar.github.io/TrueNomad/",
+        github:"https://github.com/MarinaZiembar/TrueNomad",
+        image:trueNomad,
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+    {
+        title:"Bounty Hunter",
+        subtitle:"Sitio web de canje de puntos por recompensas. Proyecto final de Acámica.",
+        web:"https://bounty-hunter-nine.vercel.app/",
+        github:"https://github.com/MarinaZiembar/BountyHunter",
+        image:"",
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator", "Api"]
+    },
+    {
+        title:"Bang!",
+        subtitle:"Revista digital. Proyecto de la materia 'Diseño 2' de la UBA.",
+        web:"https://bang-revista-digital.vercel.app/",
+        github:"https://github.com/MarinaZiembar/DG2-Salomone-TP2",
+        image:"",
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator", "Photoshop"]
+    },
+    {
+        title:"Replay!",
+        subtitle:"Playground de HTML y CSS. Creado como soporte educativo para dictado de clases.",
+        web:"https://replay-playground.vercel.app/",
+        github:"https://github.com/MarinaZiembar/replay-playground",
+        image:replay,
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+    {
+        title:"The Daily Perspective",
+        subtitle:"Portal de noticias. Segundo proyecto de Acámica.",
+        web:"https://marinaziembar.github.io/TheDailyPerspective/",
+        github:"https://github.com/MarinaZiembar/TheDailyPerspective",
+        image:theDailyPerspective,
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+    {
+        title:"Prisma",
+        comingSoon:"Próximamente",
+        subtitle:"Asistente de diseño para personas con discromatopsia.",
+        github:"https://github.com/MarinaZiembar/prisma",
+        image:prisma,
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+    {
+        title:"Mi portfolio",
+        subtitle:"Portfolio personal ❤",
+        web:"https://www.holasoymaru.com.ar",
+        github:"https://github.com/MarinaZiembar/my-portfolio",
+        image:portfolio,
+        tags:["HTML", "CSS", "Javascript", "ReactJs", "Illustrator"]
+    },
+]
